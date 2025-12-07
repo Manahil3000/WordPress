47ae8ac02b91cf41920e8f6fc3a02549a7d74431
Manahil3000 <i233000@isb.nu.edu.pk>
2025-12-07 13:56:50 +0500

    Final Building and Testing WordPress

--- tests/UI/cypress.config.js
+++ tests/UI/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost/wordpress', // your local WordPress URL
+    baseUrl: 'http://localhost/wordpress/', // your local WordPress URL
     specPattern: 'cypress/e2e/**/*.cy.js', // where your tests will live
     supportFile: false,
   },
